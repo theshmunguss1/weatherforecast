46dab759273cf17acd4ec4ae75d016ea3e2bee12
Kyle Gentry <36864779+ksgwxfan@users.noreply.github.com>
2022-07-05 23:01:00 -0400

    8ball response class and css update
    - Added new 8ball response class to prep allowing user to dictate the type of response
    - updated/simplified css stuff

--- fortuneball/8ball.js
+++ fortuneball/8ball.js
@@ -1,40 +1,58 @@
-var responses = [
-	"Absolutely!",
-	"Very<br />Doubtful",
-	"Be Careful<br />What You<br />Wish<br />For",
-	"Sorry...<br />Shake<br />again",
-	"Do cows<br />moo?",
-	"50% Chance<br />of Yes",
-	"Not Looking Good",
-	"Maybe;<br />Maybe<br />not.",
-	"No<br />Doubt!",
-	"Let Me Think<br />About it.",
-	"Positive<br />Vibes.",
-	"Sources<br />say No.",
-	"User Error.<br />Try<br />Again.",
-	"Definitely<br />Maybe",
-	"Prospects<br />Great.",
-	"My reply<br />is No",
-	"Possibly",
-	"No<br />Way",
-	"Do<br />Spiders<br />Bark?",
-	"Affirmative.",
-	"YEP!",
-	"Umm...<br />No.",
-	"Better<br />Odds In<br />Vegas.",
-	"Can't<br />predict<br />now",
-	"Seems<br />Probable"
+class FortuneAnswer {
+	constructor(ans_type, ans) {
+		this.answer_type = ans_type;
+		this.answer = ans;
+	}
+
+	get is_positive() {return this.answer_type == 0}
+	get is_negative() {return this.answer_type == 1}
+	get is_neutral() {return this.answer_type == 2}
+
+}
+
+let responses = [
+	new FortuneAnswer(0, "Absolutely!"),
+	new FortuneAnswer(1, "Very<br />Doubtful"),
+	new FortuneAnswer(2, "Be Careful<br />What You<br />Wish<br />For"),
+	new FortuneAnswer(2, "Sorry...<br />Shake<br />again"),
+	new FortuneAnswer(0, "Do cows<br />moo?"),
+	new FortuneAnswer(2, "50% Chance<br />of Yes"),
+	new FortuneAnswer(1, "Not Looking Good"),
+	new FortuneAnswer(2, "Maybe;<br />Maybe<br />not."),
+	new FortuneAnswer(0, "No<br />Doubt!"),
+	new FortuneAnswer(2, "Let Me Think<br />About it."),
+	new FortuneAnswer(0, "Positive<br />Vibes."),
+	new FortuneAnswer(1, "Sources<br />say No."),
+	new FortuneAnswer(2, "User Error.<br />Try<br />Again."),
+	new FortuneAnswer(2, "Definitely<br />Maybe"),
+	new FortuneAnswer(0, "Prospects<br />Great."),
+	new FortuneAnswer(1, "My reply<br />is No"),
+	new FortuneAnswer(2, "Possibly"),
+	new FortuneAnswer(1, "No<br />Way"),
+	new FortuneAnswer(0, "Do<br />Spiders<br />Bark?"),
+	new FortuneAnswer(0, "Affirmative."),
+	new FortuneAnswer(0, "YEP!"),
+	new FortuneAnswer(0, "YES."),
+	new FortuneAnswer(1, "NO."),
+	new FortuneAnswer(1, "Umm...<br />No."),
+	new FortuneAnswer(1, "Better<br />Odds In<br />Vegas."),
+	new FortuneAnswer(2, "Can't<br />predict<br />now"),
+	new FortuneAnswer(0, "Seems<br />Probable"),
 ];
 
-var triangle = document.getElementById("triangle");
-var answer = document.getElementById("response");
-var b = document.getElementById("ball");
-var answerOpacity = 0;
-var alreadyAsked = false;
-var question = document.getElementById("question");
+let triangle = document.getElementById("triangle");
+let answer = document.getElementById("response");
+let ball = document.getElementById("ball");
+let answerOpacity = 0;
+let alreadyAsked = false;
+let question = document.getElementById("question");
+
+// Control the type of answer allowed/given
+let controlledAnswerType = "any";
 
 // Add keyboard event listener for ENTER
 question.addEventListener("keydown", enter_pressed);
+
 
 function enter_pressed(event) {
 	//console.log(event);
@@ -48,12 +66,12 @@ function enter_pressed(event) {
 
 function ask() {
 	if (alreadyAsked == false) {
-		alreadyAsked = true;
-		answer.innerHTML = "";
-		answerOpacity = 0;
+		alreadyAsked = true; 	// prevent invocation if active
+		answer.innerHTML = "";	// clear previous answer
+		answerOpacity = 0;		// 'hide' the answer
 		answer.style.opacity = `${answerOpacity}%`;
 		triangle.style.opacity = `${answerOpacity}%`;
-		var time = 100;
+		let time = 100;
 		if (question.value.length >= 1 && question.value.substr(question.value.length-1) != "?") {
 			question.value += "?";
 		}
@@ -68,11 +86,11 @@ function ask() {
 	}
 }
 function shakeleft() {
-	b.style.left = "45%";
+	ball.style.left = "45%";
 }
 
 function shakeright() {
-	b.style.left = "55%";
+	ball.style.left = "55%";
 }
 
 // document.getElementById("description").addEventListener(
@@ -90,11 +108,20 @@ function r(min=0, max=24) {
 
 
 function display() {
-	var x = 0 + Math.floor(Math.random() * responses.length);
+	// build valid responses
+	let validResponses = [];
+	for (let resp of responses) {
+		if (controlledAnswerType == "any" || resp[`is_${controlledAnswerType}`]) {
+			validResponses.push(resp);
+		}
+	}
+
+	let x = Math.floor(Math.random() * validResponses.length);
 	// console.log(x, responses[x]);
-	b.style.left = "50%"; 	// Re-center image
-	answer.innerHTML = responses[x]; //Displays random response
-	rvlanswer = setInterval(reveal,80);
+	// ball.style.left = "50%"; 	// Re-center image
+	ball.style.left = "initial"; 	// Re-center image
+	answer.innerHTML = validResponses[x].answer; //Displays random response
+	rvlanswer = setInterval(reveal, 80);
 }
 
 function reveal() {
