7a42207dfc4b545ac251cc03e4dc6f4e9fc29853
Kyle Gentry <36864779+ksgwxfan@users.noreply.github.com>
2022-08-03 17:35:11 -0400

    Customization Update
    User can now update colors, and options and colors are remembered via localStorage variable

--- bingo/bingo_fullscreen.js
+++ bingo/bingo_fullscreen.js
@@ -21,6 +21,8 @@ const button_box = document.getElementById("button-box");
 const container_last_called = document.getElementById("container-last-called");
 const new_game_dialog = document.getElementById("new-game-dialog");
 const options_dialog = document.getElementById("options");
+const color_options = document.getElementById("color-options");
+const call_buffer_toggle = document.getElementById("input-call-buffer-toggle");
 const call_display = document.getElementById("call-display");
 const newcall_btn = document.getElementById("newcall-button");
 
@@ -32,8 +34,119 @@ const bingo = {
 	caller_side : "right",
 	caller_placement : "lower",
 	recent_call_dir : "lower",
-	called_background_color: "yellow",
-	called_text_color: "black",
+
+	// background_color: "#FFFFFF",
+	// text_color: "#000000",
+	called_background_color: "#FFFF00",
+	called_text_color: "#000000",
+	new_call_outline_color: "#000000",
+
+	default_caller_side: "right",
+	default_caller_placement: "lower",
+	default_recent_call_dir: "lower",
+	default_called_background_color: "#FFFF00",
+	default_called_text_color: "#000000",
+	default_background_color: "#FFFFFF",
+	default_text_color: "#000000",
+	default_call_display_color: "#000080",
+	default_recent_calls_color: "#808080",
+	default_new_call_outline_color: "#000000",
+}
+
+// Test to see if localStorage is available
+// if not available...create a placeholding variable
+if (Object.keys(window).includes("localStorage") == false) {
+	class STRG {
+		constructor() {}
+
+		setItem(k, v) {
+			this[k] = v;
+		}
+
+		getItem(k) {
+			return this[k];
+		}
+
+		clear() {
+
+		}
+
+		removeItem(i) {
+
+		}
+	}
+	localStorage = new STRG();
+	// const localStorage = {};
+}
+// if localStorage is available, try loading from it
+else {
+	// Location of visuals
+	if (localStorage.getItem("caller_side") != null) {
+		document.getElementById(
+			`input-caller-side-${
+				localStorage.getItem('caller_side')
+			}`
+		).click();
+	}
+	if (localStorage.getItem("caller_placement") != null) {
+		document.getElementById(
+			`input-caller-placement-${
+				localStorage.getItem('caller_placement')
+			}`
+		).click();
+	}
+	if (localStorage.getItem("recent_call_dir") != null) {
+		document.getElementById(
+			`input-recent-call-dir-${
+				localStorage.getItem('recent_call_dir')
+			}`
+		).click();
+	}
+	if (localStorage.getItem("call_buffer_toggle") != null) {
+		let toggle_valu = localStorage.getItem("call_buffer_toggle");
+		while (
+			call_buffer_toggle.checked.toString() !=
+			toggle_valu
+		) {
+			call_buffer_toggle.click();
+		}
+	}
+
+	// COLORS
+	if (localStorage.getItem("background_color") != null) {
+		change_board_color(localStorage.getItem("background_color"));
+		document.getElementById("color-bg").value = localStorage.getItem("background_color");
+	}
+	if (localStorage.getItem("text_color") != null) {
+		change_board_color(localStorage.getItem("text_color"), "fg");
+		document.getElementById("color-text").value = localStorage.getItem("text_color");
+	}
+	if (localStorage.getItem("called_background_color") != null) {
+		change_called_color(localStorage.getItem("called_background_color"));
+		document.getElementById("color-called-bg").value = localStorage.getItem("called_background_color");
+	}
+	if (localStorage.getItem("called_text_color") != null) {
+		change_called_color(localStorage.getItem("called_text_color"), "fg");
+		document.getElementById("color-called-text").value = localStorage.getItem("called_text_color");
+	}
+	if (localStorage.getItem("call_display_color") != null) {
+		change_call_display_color(
+			localStorage.getItem("call_display_color")
+		);
+		document.getElementById("color-call-display").value = localStorage.getItem("call_display_color");
+	}
+	if (localStorage.getItem("recent_calls_color") != null) {
+		change_recent_calls_color(
+			localStorage.getItem("recent_calls_color")
+		);
+		document.getElementById("color-recent-calls").value = localStorage.getItem("recent_calls_color");
+	}
+	if (localStorage.getItem("new_call_outline_color") != null) {
+		change_new_call_outline_color(
+			localStorage.getItem("new_call_outline_color")
+		);
+		document.getElementById("color-new-call-outline").value = localStorage.getItem("new_call_outline_color");
+	}
 }
 
 _today = new Date();
@@ -41,7 +154,7 @@ _today = new Date();
 if (_today.getMonth() == 11 ||
     _today.getMonth() == 6 && (_today.getDate() == 26 || _today.getDate() == 27)
 ) {
-		ChristmasTimeIsHere();
+	ChristmasTimeIsHere();
 }
 
 // CHRISTMAS THEME
@@ -82,13 +195,21 @@ function new_call() {
 	// console.log(MIN + Math.floor(Math.random() * (MAX-MIN+1)));
 
 	// prevent accidental/inadvertent call
-	call_protect(2);
+	if (call_buffer_toggle.checked) {		
+		call_protect(2);
+	}
 
 	// random bingo space (index)
 	let r = Math.floor(Math.random() * bingo.possibilities.length);
 
 	// Note the space being called
+	// record
 	bingo.called.unshift(bingo.possibilities[r]);
+
+	// make sure only the most-recent call has a box highlight around it
+	if (bingo.called.length > 1) {
+		document.getElementById(bingo.called[1]).style.outline = "none";
+	}
 
 	// Display the space just called
 	call_display.innerText = bingo.possibilities[r].toUpperCase();
@@ -99,6 +220,8 @@ function new_call() {
 	selected.style.backgroundColor = bingo.called_background_color;
 	selected.style.color = bingo.called_text_color;
 	selected.style.fontWeight = "bold";
+	selected.style.fontSize = "5.5vmin";
+	selected.style.outline = `1.25vmin double ${bingo.new_call_outline_color}`;
 
 	// narrow possibilities array
 	bingo.possibilities = [].concat(
@@ -132,6 +255,8 @@ function new_game() {
 		document.getElementById(space).style.backgroundColor = "initial";
 		document.getElementById(space).style.color = "initial";
 		document.getElementById(space).style.fontWeight = "initial";
+		document.getElementById(space).style.fontSize = "4.8vmin";
+		document.getElementById(space).style.outline = "none";
 	}
 
 	// Reset called list
@@ -149,6 +274,7 @@ function new_game() {
 
 function change_side(dir) {
 	bingo.caller_side = dir;
+	localStorage.setItem("caller_side", dir);
 	if (dir == "left") {
 		game_columns.insertBefore(
 			controls_column,
@@ -163,6 +289,7 @@ function change_side(dir) {
 
 function change_call_btn_placement(dir) {
 	bingo.caller_placement = dir;
+	localStorage.setItem("caller_placement", dir);
 	if (dir == "upper") {
 		controls_frame.insertBefore(
 			newcall_btn,
@@ -179,6 +306,7 @@ function change_call_btn_placement(dir) {
 
 function change_recent_calls_direction(dir) {
 	bingo.recent_call_dir = dir;
+	localStorage.setItem("recent_call_dir", dir);
 
 	// Remove all nodes
 	controls_frame.removeChild(container_last_called);
@@ -207,11 +335,115 @@ function change_recent_calls_direction(dir) {
 	}
 }
 
+function change_board_color(new_color, which="bg") {
+	// Background color
+	if (which == "bg") {
+		document.body.style.backgroundColor = new_color;
+		container.style.backgroundColor = "#FFFFFF" + "64";
+		localStorage.setItem("background_color", new_color);
+	}
+	// Main text color
+	else {
+		document.body.style.color = new_color;
+		localStorage.setItem("text_color", new_color);
+	}
+}
 
+function change_called_color(new_color, which="bg") {
+	// Called background
+	if (which == "bg") {
+		localStorage.setItem("called_background_color", new_color);
+		bingo.called_background_color = new_color;
+		for (space of bingo.called) {
+			document.getElementById(space).style.backgroundColor = new_color;
+		}
+	}
+	// called text color
+	else {
+		localStorage.setItem("called_text_color", new_color);
+		bingo.called_text_color = new_color;
+		for (space of bingo.called) {
+			document.getElementById(space).style.color = new_color;
+		}
+	}
+}
 
+function change_call_display_color(new_color) {
+	localStorage.setItem("call_display_color", new_color);
+	call_display.style.color = new_color;
+}
 
+function change_recent_calls_color(new_color) {
+	localStorage.setItem("recent_calls_color", new_color);
+	for (let recent of document.getElementsByClassName("recent-call")) {
+		recent.style.color = new_color;
+	}
+}
 
+function change_new_call_outline_color(new_color) {
+	localStorage.setItem("new_call_outline_color", new_color);
+	bingo.new_call_outline_color = new_color;
+	if (bingo.called.length > 0) {
+		document.getElementById(bingo.called[0]).style.outline = `1.25vmin double ${bingo.new_call_outline_color}`;
+	}
+}
 
+function placement_reset() {
+	document.getElementById(
+		`input-caller-side-${bingo.default_caller_side}`
+	).click();
+	document.getElementById(
+		`input-caller-placement-${bingo.default_caller_placement}`
+	).click();
+	document.getElementById(
+		`input-recent-call-dir-${bingo.default_recent_call_dir}`
+	).click();
+
+	while (call_buffer_toggle.checked == false) {
+		call_buffer_toggle.click();
+	}
+
+	for (let item of [
+		"caller_side", "caller_placement", "recent_call_dir", 
+		"call_buffer_toggle"
+	]) {
+		localStorage.removeItem(item);
+	}
+
+}
+
+function color_reset() {
+	// background
+	change_board_color(bingo.default_background_color);
+	document.getElementById("color-bg").value = bingo.default_background_color;
+
+	change_board_color(bingo.default_text_color, "fg");
+	document.getElementById("color-text").value = bingo.default_text_color;
+
+	change_called_color(bingo.default_called_background_color);
+	document.getElementById("color-called-bg").value = bingo.default_called_background_color;
+
+	change_called_color(bingo.default_called_text_color, "fg");
+	document.getElementById("color-called-text").value = bingo.default_called_text_color;
+
+	change_call_display_color(bingo.default_call_display_color);
+	document.getElementById("color-call-display").value = bingo.default_call_display_color;
+
+	change_recent_calls_color(bingo.default_recent_calls_color);
+	document.getElementById("color-recent-calls").value = bingo.default_recent_calls_color;
+
+	change_new_call_outline_color(bingo.default_new_call_outline_color);
+	document.getElementById("color-new-call-outline").value = bingo.default_new_call_outline_color;
+
+	for (let item of [
+		"background_color", "text_color", "called_background_color",
+		"called_text_color", "call_display_color", "recent_calls_color",
+		"new_call_outline_color",
+	]) {
+		localStorage.removeItem(item);
+	}
+
+}
 
 
 
