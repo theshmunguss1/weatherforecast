be28a003e9c1af378b09bd051572f47023f9739d
Kyle Gentry <36864779+ksgwxfan@users.noreply.github.com>
2023-09-12 22:01:12 -0400

    last_updated js var 2023-09-12T22:00 EDT

--- unofficial_hurdat2/last_updated.js
+++ unofficial_hurdat2/last_updated.js
@@ -1,1 +1,1 @@
-document.getElementById("last-updated").innerText = "Stats Last Updated: 11 Sep 2023 22:00 EDT";
+document.getElementById("last-updated").innerText = "Stats Last Updated: 12 Sep 2023 22:00 EDT";
