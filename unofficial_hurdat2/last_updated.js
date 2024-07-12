606de06b3ff5fc7fc8c18423b21eb490626a453a
Kyle Gentry <36864779+ksgwxfan@users.noreply.github.com>
2024-07-11 22:00:40 -0400

    last_updated js var 2024-07-11T22:00 EDT

--- unofficial_hurdat2/last_updated.js
+++ unofficial_hurdat2/last_updated.js
@@ -1,1 +1,1 @@
-document.getElementById("last-updated").innerText = "Stats Last Updated: 10 Jul 2024 22:00 EDT";
+document.getElementById("last-updated").innerText = "Stats Last Updated: 11 Jul 2024 22:00 EDT";
